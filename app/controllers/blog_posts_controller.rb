d3c384e3a75c4bbbf8e2905daed5dffe369bce75
Ignasi De La Madrid <ignacio@wellnessfx.com>
2011-11-19 16:08:00 -0800

    interface with sales force to show the blog posts index

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,9 +1,24 @@
+#SalesForce API
+#<Blog_Post__c:0x00000102d64630
+  #@Id="a00U0000001jpgAIAQ",
+  #@OwnerId="005U0000000dn0hIAA",
+  #@IsDeleted=false,
+  #@Name="Hello World 3",
+  #@CreatedDate=Sat, 19 Nov 2011 20:42:08 +0000,
+  #@CreatedById="005U0000000dn0hIAA",
+  #@LastModifiedDate=Sat, 19 Nov 2011 20:42:08 +0000,
+  #@LastModifiedById="005U0000000dn0hIAA",
+  #@SystemModstamp=Sat, 19 Nov 2011 20:42:08 +0000,
+  #@LastActivityDate=nil,
+  #@Body__c="testing",
+  #@Status__c="Published",
+  #@Type__c="Text">]
+
 class BlogPostsController < ApplicationController
-	include Databasedotcom::Rails::Controller
+  include Databasedotcom::Rails::Controller
 
   def index
-  	debugger
-  	@blogposts = Blog_Post__c.all
+    @blog_posts = Blog_Post__c.all
   end
 
   def new
